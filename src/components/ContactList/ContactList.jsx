abfba9064c870b4f9d173d01eb3286f694b1de95
Maks Novakov <maksnovakov78@gmail.com>
2022-12-11 16:23:25 +0200

    2

---
+++ src/components/ContactList/ContactList.jsx
@@ -0,0 +1,44 @@
+// import PropTypes from 'prop-types';
+import './ContactList.module.css';
+import { useDispatch, useSelector } from 'react-redux';
+import { getContacts, getFilter, deleteContact } from '../../redux/slice';
+
+export const ContactList = () => {
+  const dispatch = useDispatch();
+  const contacts = useSelector(getContacts);
+  const filter = useSelector(getFilter);
+
+  const findContacts = () => {
+    const normalizedFilter = filter.toLowerCase();
+    return contacts.filter(contact =>
+      contact.name.toLowerCase().includes(normalizedFilter)
+    );
+  };
+
+  const filteredContacts = findContacts();
+
+  return (
+    <ul>
+      {filteredContacts.map(({ id, name, number }) => {
+        return (
+          <li key={id}>
+            <p>
+              {name}: {number}
+            </p>
+            <button type="button" onClick={() => dispatch(deleteContact(id))}>
+              Delete
+            </button>
+          </li>
+        );
+      })}
+    </ul>
+  );
+};
+
+// ContactList.propTypes = {
+//   contacts: PropTypes.array,
+//   name: PropTypes.string,
+//   number: PropTypes.string,
+//   id: PropTypes.string,
+//   deleteContact: PropTypes.func,
+// };
